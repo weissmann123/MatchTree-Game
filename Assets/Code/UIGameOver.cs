c05ebfe3331b7c09e99337d66945b89c3bdaffb5
Michael <c14180035@john.petra.ac.id>
2021-12-11 19:18:02 +0700

    no message

---
+++ Assets/Code/UIGameOver.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameOver = false;
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    [Header("UI")]
+    public UIGameOver GameOverUI;
+    private bool isGameOver;
+    public void GameOver()
+    {
+        isGameOver = true;
+        ScoreManager.Instance.SetHighScore();
+        GameOverUI.Show();
+    }
+}
